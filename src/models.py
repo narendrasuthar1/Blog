2baed924091a93b9c536d6cbca43413906c2bd6c
Admin <narendrasuthar@outlook.com>
2016-10-01 16:25:47 +0530

    My Initial Commit

---
+++ src/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Post(models.Model):
+    tittle=models.CharField(max_length=60)
+    published_at=models.DateTimeField()
+    desciption=models.TextField()
+    author_name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.tittle
+
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post)
+    description=models.TextField()
+    published_at=models.DateTimeField(default=timezone.now())
+
+    #def_str_(self)
+
